d8adcca57ee080652a2e53279b2f3c64dba48f64
Logan Sulpizio <logan.sulpizio@widetech.com>
2025-06-23 22:50:53 +0200

    Improve parameters of live indexing

--- yam_indexing_module/main_indexing.py
+++ yam_indexing_module/main_indexing.py
@@ -12,8 +12,8 @@ from yam_indexing_module.logging.logging_config import setup_logging
 
 
 BLOCK_TO_RETRIEVE = 3                   # Number of block to retrieve from the W3 RPC by HTTP request 
-COUNT_BEFORE_RESYNC = 200               # Number of retrieve before resynchronizing to the latest block
-BLOCK_BUFFER = 4                        # Gap between the latest block available and what is actually retrieve
+COUNT_BEFORE_RESYNC = 100               # Number of retrieve before resynchronizing to the latest block
+BLOCK_BUFFER = 5                        # Gap between the latest block available and what is actually retrieve
 TIME_TO_WAIT_BEFORE_RETRY = 1.5         # time to wait before retry when RPC is not available
 MAX_RETRIES_PER_BLOCK_RANGE = 6         # Number of time the request will be retried when it has failed before changing the RPC
 COUNT_PERIODIC_BACKFILL_THEGRAPH = 960  # Number of iteration before backfilling the blocks into the DB the blocks of the last few hours (with TheGraph)
